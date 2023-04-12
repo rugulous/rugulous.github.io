f8d0fbc6623979071679a1e1ec09094980862985
Luke Taylor <luke@circleleasing.co.uk>
2023-04-12 16:02:30 +0100

    Add pricing

--- village/menu/src/components/Item.vue
+++ village/menu/src/components/Item.vue
@@ -23,8 +23,19 @@ function isInStock(item) {
 </script>
 
 <template>
-	<div class="mb-3">
+	<div class="mb-3" :class="{'no-stock': !isInStock(item)}">
 		<p class="lead mb-0">{{ item.title }} <span v-if="!isInStock(item)" class="text-danger">Out of Stock</span></p>
-		<small class="text-secondary">Contains {{ formatIngredients(item.ingredients) }}</small>
+		<small class="d-block text-secondary mb-2">Contains {{ formatIngredients(item.ingredients) }}</small>
+
+		
+			<p class="m-0" v-for="variant in item.variants"><strong>{{ variant.name }}</strong> {{ variant.price }}</p>
+
 	</div>
 </template>
+
+<style scoped>
+	.no-stock{
+		border: 1px solid #F00;
+		background-color: rgba(255,0,0,0.1);
+	}
+</style>
