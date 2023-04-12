f4b74f72ab340949570e998f4167446dd31b6263
Luke Taylor <luke@circleleasing.co.uk>
2023-04-12 16:38:11 +0100

    Add button to bottom of page

--- village/menu/source/src/components/Menu.vue
+++ village/menu/source/src/components/Menu.vue
@@ -18,5 +18,20 @@ data.loaded = true;
 		<h1 class="display-2 text-center mb-5 pt-3">Menu</h1>
 
 		<Category v-for="key in Object.keys(data.menu)" :title="key" :items="data.menu[key]" />
+
+		<button class="btn btn-light chooser">Help Me Decide!</button>
 	</div>
 </template>
+
+<style scoped>
+	.container{
+		padding-bottom: 2rem;
+	}
+
+	.chooser{
+		position: fixed;
+		bottom: 0;
+		width: 100%;
+		left: 0;
+	}
+</style>
