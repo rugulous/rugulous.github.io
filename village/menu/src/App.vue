f6f0d513cdd3571d91a4758405ac2393757f81af
Luke Taylor <luke@circleleasing.co.uk>
2023-04-12 14:26:53 +0100

    Start Vue project

---
+++ village/menu/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Category from './components/Category.vue';
+
+let menu = [];
+
+fetch('menu.json').then(res => res.json()).then(json => {
+  console.log(json);
+});
+</script>
+
+<template>
+  <h1>Menu</h1>
+  <Category title="Soft Drinks" :items="menu" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ village/menu/src/components/Category.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  items: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <h2>{{ title }}</h2>
+</template>
